967a4ad4eacaface953f1e25651ebb3ff76a64e4
Kyle-Stacklin <77754715+Kyle-Stacklin@users.noreply.github.com>
2021-12-04 18:18:05 -0800

    Added view forms modal for Admin users

--- pages/reminder_page.php
+++ pages/reminder_page.php
@@ -15,15 +15,6 @@
             }
         }
 
-        if(empty($_POST['password'])){
-            $errors['password'] = 'Password is required';
-        } else{
-            $password = $_POST['password'];
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                $errors['email'] = 'Password must follow restrictions';
-            }
-        }
-
         if(array_filter($errors)){
             // echo 'errors in form';
         } else {
@@ -39,10 +30,12 @@
 <html>
 <head>
     <link href="../css/styles.css" rel="stylesheet">	
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">  
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </head>
-<body class="grey lighten-4">
+<body class="">
     
 	<section class="container grey-text" style="display: flex;">
 		<form class="white login-form" action="sign_up_page.php" method="POST">
@@ -56,13 +49,51 @@
 			<div class="center">
                 <div class="green-text"><?php echo $creation['success']; ?></div>
 				<input type="submit" name="submit" value="Set deadline" class="btn brand z-depth-0">
-				<br />
+				<br /><br />
+                <hr style="border-top: 1px dashed grey;">
                 <br />
-
 			</div>
+
+            <div class="center">
+                <a class="btn modal-trigger brand z-depth-0" href="#view-forms-modal">
+                View all forms
+                </a>
+            </div>
 		</form>
+        
 
 	</section>
 
+    
+
+    <!-- View Forms Modal Structure -->
+    <div id="view-forms-modal" class="modal">
+        <div class="modal-content grey-text">
+            <h4 class="brand-text text-bold" id="view-edit-modal-title"><strong>All book order forms</strong></h4>
+            <hr>
+
+        </div>
+
+        <div class="modal-footer">
+            <a href="#!" style="margin-right: 10px;" class="modal-action 
+                modal-close waves-effect waves-green 
+                btn green lighten-1">
+                Finalize orders
+            </a>
+
+            <a href="#!" class="modal-action 
+                modal-close btn brand lighten-1">
+                Close
+            </a>
+        </div>
+    </div>
+
 </body>
+
+    <script>
+        $(document).ready(function () {
+            $('.modal').modal();
+        }
+        )
+    </script>
 </html>
