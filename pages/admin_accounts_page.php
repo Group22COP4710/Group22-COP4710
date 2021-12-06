970579da6fcca56645913c659d6e5c2fc9e07488
Kyle-Stacklin <77754715+Kyle-Stacklin@users.noreply.github.com>
2021-12-05 18:15:27 -0800

    Create admin endpoint hook up

--- pages/admin_accounts_page.php
+++ pages/admin_accounts_page.php
@@ -1,4 +1,60 @@
 <?php 
+$email = $name = $password = '';
+$errors = array('email' => '', 'name' => '', 'password' => '');
+$creation = array('success' => '');
+
+// connect to the database
+$conn = mysqli_connect('localhost', 'user', 'password', 'final');
+
+if(isset($_POST['submit'])){
+    
+    // check email
+    if(empty($_POST['email'])){
+        $errors['email'] = 'An email is required';
+    } else{
+        $email = $_POST['email'];
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errors['email'] = 'Email must be a valid email address';
+        }
+    }
+
+    if(empty($_POST['name'])){
+        $errors['name'] = 'An name is required';
+    } else{
+        $name = $_POST['name'];
+    }
+
+    if(empty($_POST['password'])){
+        $errors['password'] = 'Password is required';
+    } else{
+        $password = $_POST['password'];
+    }
+
+    if(array_filter($errors)){
+        // echo 'errors in form';
+    } else {
+        // echo 'form is valid';
+        // echo $email . " " . $password;
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        else{
+            // echo 'connect successful';
+            
+            $sql = "INSERT INTO Users (email, Name, Password, User_Type) VALUES ('$email', '$name', '$password', 'Admin')";
+    
+            if (mysqli_query($conn, $sql)) {
+                // echo "New record created successfully";
+                $creation['success'] = 'Account created successfully';
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
+        }
+    }	
+
+} // end POST check
 
 ?>
 
@@ -109,10 +165,13 @@
                 <h4 class="brand-text text-bold" id="view-edit-modal-title"><strong>Create new admin account</strong></h4>
 				<hr>
 
-                <form class="white login-form" action="sign_up_page.php" method="POST">
+                <form class="white login-form" action="admin_accounts_page.php" method="POST">
                     <label>Email</label>
                     <input type="text" name="email" value="">
                     <div class="red-text"><?php echo $errors['email']; ?></div>
+                    <label>Name</label>
+                    <input type="text" name="name" value="">
+                    <div class="red-text"><?php echo $errors['name']; ?></div>
                     <label>Password</label>
                     <input type="text" name="password" value="">
                     <div class="red-text"><?php echo $errors['password']; ?></div>
@@ -120,22 +179,22 @@
                     <div class="center">
                         <div class="green-text"><?php echo $creation['success']; ?></div>
                     </div>
+
+                    <div class="modal-footer">
+                        
+                        <input type="submit" name="submit" value="Create admin" class="btn brand z-depth-0">
+        
+
+                        <a href="#!" class="modal-action 
+                            modal-close waves-effect 
+                            btn brand lighten-1">
+                            Close
+                        </a>
+                    </div>
                 </form>
             </div>
   
-            <div class="modal-footer">
-				<a href="#!" style="margin-right: 10px;" class="modal-action 
-                    modal-close waves-effect waves-green 
-                    btn green lighten-1">
-                    Create admin
-                </a>
-
-                <a href="#!" class="modal-action 
-                    modal-close waves-effect 
-                    btn brand lighten-1">
-                    Close
-                </a>
-            </div>
+            
         </div>
     </div>
 
