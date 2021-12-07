b794abe9bb40439504f042a8d5c0fecd48f994c8
andregr13 <89662452+andregr13@users.noreply.github.com>
2021-12-06 22:52:31 -0500

    create request form id cookie

--- homepage.php
+++ homepage.php
@@ -1,5 +1,20 @@
 <?php 
-
+	$conn = mysqli_connect("localhost", "user", "password", "final"); 	
+	if(!$conn){
+		echo 'Connection error: '. mysqli_connect_error();
+	}
+	else
+	{
+		$result = mysqli_query($conn, "SELECT Req_ID FROM RequestForms WHERE USER_ID = {$_COOKIE['User_ID']}");
+		if ($row = mysqli_fetch_assoc($result))
+		{
+			setcookie("Req_ID",$row["Req_ID"],time()+3600 , '/' );	
+		}
+		else
+		{
+			echo "Request Form not found";	
+		}
+	}
 ?>
 
 <!DOCTYPE html>
