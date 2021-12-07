c4f37a347f9d3b916a32b9cc3d4647f0c98a5776
Clutch-Hinkle <63745665+Clutch-Hinkle@users.noreply.github.com>
2021-12-06 21:05:37 -0500

    Updated getBookRequest

--- api/getBookOrder.php
+++ api/getBookOrder.php
@@ -1,66 +1,65 @@
 <?php
 
-	$inData = getRequestInfo();
+	//$inData = getRequestInfo();
 
-	$oid = $inData["Order_ID"];
+	$oid = $_Post["Order_ID"];
+	$retVal = [];
 	
-	$conn = new mysqli("localhost", "user", "password", "final"); 	
-	if( $conn->connect_error )
+	$conn = mysqli_connect("localhost", "user", "password", "final"); 	
+	if( $error mysqli_connect_error())
 	{
-		returnError(500, $conn->connect_error );
+		$retVal = array("Error" => $error );
 	}
 	else
 	{
 
-		$result = $conn->query("SELECT * FROM bookOrder WHERE Order_ID={$oid}");
+		$result = mysqli_query($conn,"SELECT * FROM bookOrder WHERE Order_ID={$oid}");
 		
-		if ($row = $result->fetch_assoc())
+		if ($row = mysqli_fetch_assoc($result))
 		{
-			returnData($row["Order_ID"],$row["Title"],$row["ISBN"],$row["Author"],$row["Edition"],$row["Publisher"],$row["Req_ID"]);
+			$retVal = returnData($row["Order_ID"],$row["Title"],$row["ISBN"],$row["Author"],$row["Edition"],$row["Publisher"],$row["Req_ID"]);
 		}
 		else 
 		{
-			returnError(500, "No Order found");
+			$retVal = array("Error"=>"No orders found");
 		}
 
 		$conn->close();
 	}
 	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+	// function getRequestInfo()
+	// {
+	// 	return json_decode(file_get_contents('php://input'), true);
+	// }
 
-	function sendJSON( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+	// function sendJSON( $obj )
+	// {
+	// 	header('Content-type: application/json');
+	// 	echo $obj;
+	// }
 	
 	function returnData($oid,$title,$isbn,$author,$edition,$pub,$reqid)
 	{
 		$retValue = array(
-			"Order"=>array(
 				"Order_ID"=>$oid,
 				"Title"=>$title,
 				"ISBN"=>$isbn,
 				"Author"=>$author,
 				"Edition"=>$edition,
 				"Publisher"=>$pub,
-				"Req_ID"=>$reqid),
-			"Error"=>array("code"=>200));
+				"Req_ID"=>$reqid);
 		
-		sendJSON(json_encode($retValue, JSON_FORCE_OBJECT));
+		//sendJSON(json_encode($retValue, JSON_FORCE_OBJECT));
 	}
 	
-	function returnError($code, $err )
-	{
-		$retValue = array(
-			"Error"=>array(
-				"code"=>$code,
-				"Message"=>$err));
+	// function returnError($code, $err )
+	// {
+	// 	$retValue = array(
+	// 		"Error"=>array(
+	// 			"code"=>$code,
+	// 			"Message"=>$err));
 		
-		sendJSON(json_encode($retValue,JSON_FORCE_OBJECT));
-	}
+	// 	sendJSON(json_encode($retValue,JSON_FORCE_OBJECT));
+	// }
 	
 ?>
