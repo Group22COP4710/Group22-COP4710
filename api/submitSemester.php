f8f829fe4f2e49a4c1eac5347532119520d6c838
andregr13 <89662452+andregr13@users.noreply.github.com>
2021-12-07 13:19:59 -0500

    finalize submit semester

--- api/submitSemester.php
+++ api/submitSemester.php
@@ -1,21 +1,18 @@
 <?php
 
-	$inData = getRequestInfo();
+	$semid = $_COOKIE["Sem_ID"];
 
-	$semid = $inData["Sem_ID"];
-	$newYear;
-	$newSeason;
 	
-	$conn = new mysqli("localhost", "user", "password", "final"); 	
-	if( $conn->connect_error )
+	$conn = mysqli_connect("localhost", "user", "password", "final"); 	
+	if( $error mysqli_connect_error())
 	{
-		returnError(500, $conn->connect_error);
+		echo $error ;
 	}
 	else
 	{
-		$result = $conn->query("SELECT Season, Year FROM Semester WHERE Sem_ID = {$semid}");
+		$result = mysqli_query($conn,"SELECT Season, Year FROM Semester WHERE Sem_ID = {$semid}");
 		
-		if ($row = $result->fetch_assoc())
+		if ($row = mysqli_fetch_assoc($result))
 		{
 			if ($row["Season"] == "Fall")
 			{
@@ -29,58 +26,31 @@
 			}
 			else
 			{
-				returnError(500, "Database Error");
+				echo "Database Error";
 			}
 		}
 		else
 		{
-			returnError(500, "Invalid Request");
+			echo "Invalid Request";
 		}
 		
-		$result = $conn->query("UPDATE Semester SET Current = false WHERE Sem_ID = {$semid}");
+		$result = mysqli_query($conn,"UPDATE Semester SET Current = false WHERE Sem_ID = {$semid}");
 		if ($result)
 		{
-			$result = $conn->query("INSERT INTO Semester (Season, Year, Current) VALUES ('{$newSeason}',{$newYear},true)");
-			returnData($newSeason, $newYear);
+			$result = mysqli_query($conn,"INSERT INTO Semester (Season, Year, Current) VALUES ('{$newSeason}',{$newYear},true)");
+			
+			include('../api/getCurrentSemester.php');
+			setcookie("Sem_ID",$semester["Sem_ID"], time()+3600, '/');
+			setcookie("Year",$semester["Year"], time()+3600, '/');
+			setcookie("Deadline",null, time()+3600, '/');
+			setcookie("Season",$semester["Season"], time()+3600, '/');
 		}
 		else
 		{
-			returnError(500, "Database Error");
+			echo "Database Error";
 		}
 		
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendJSON( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnData($season, $year)
-	{
-		$retValue = array(
-			"data"=>array(
-				"Season"=>$season,
-				"Year"=>$year),
-			"Error"=>array("code"=>200));
-		
-		sendJSON(json_encode($retValue, JSON_FORCE_OBJECT));
-	}
-	
-	function returnError($code, $err )
-	{
-		$retValue = array(
-			"Error"=>array(
-				"code"=>$code,
-				"Message"=>$err));
-		
-		sendJSON(json_encode($retValue, JSON_FORCE_OBJECT));
+		mysqli_close($conn);
 	}
 	
 ?>
