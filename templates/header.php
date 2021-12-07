b2b12a34d17de67abdd13873b3950123189ae7a7
Kyle-Stacklin <77754715+Kyle-Stacklin@users.noreply.github.com>
2021-12-07 02:42:34 -0500

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
 <body class="grey lighten-4">
 	<nav class="white z-depth-0">
     <div class="container">
-      <a href="../homepage.php" class="brand-logo brand-text">UCF Book Order</a>
+      <a href="../page/book_requests.php" class="brand-logo brand-text">UCF Book Order</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
         <!-- <li><a href="/pages/book_requests.php" class="btn brand z-depth-0" name="book-requests">Book Requests</a></li> -->
 		    <li><a href="/pages/accounts_page.php" class="btn brand z-depth-0" name="accounts">Accounts</a></li>
