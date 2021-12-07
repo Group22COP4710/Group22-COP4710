7a2b0f5d6b6ed515f9d768a306af466a26ae1644
Ryan Steinmetz <ryansteinmetz@Ryans-MacBook-Pro.local>
2021-12-07 14:00:02 -0500

    Adding Delete admin account button

--- pages/admin_accounts_page.php
+++ pages/admin_accounts_page.php
@@ -162,6 +162,14 @@ if(isset($_POST['createAdmin'])){
                     Create new admin account
                     </a>
                 </div>
+
+                <br />
+
+                <div class="center">
+                    <a class="waves-effect waves-light btn modal-trigger brand z-depth-0" href="#delete-admin-modal">
+                    Delete admin account
+                    </a>
+                </div>
 		
 		<br />
 
