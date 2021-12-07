0784cc1a688f1bbdb1d9788cefd01782d82806e9
andregr13 <89662452+andregr13@users.noreply.github.com>
2021-12-07 00:48:06 -0500

    Update setDeadline.php

--- api/setDeadline.php
+++ api/setDeadline.php
@@ -1,50 +1,28 @@
 <?php
 
-	$inData = getRequestInfo();
 
-	$deadline = $inData["Deadline"];
+
+	$deadline = date("Y-m-d", strtotime$_POST["Deadline"]);
 	
-	$conn = new mysqli("localhost", "user", "password", "final"); 	
-	if( $conn->connect_error )
+	$conn = mysqli_connect("localhost", "user", "password", "final"); 	
+	if($error = mysqli_connect_error() )
 	{
-		returnError(500, $conn->connect_error );
+		echo $error;
 	}
 	else
 	{
-		$result = $conn->query("UPDATE Semester SET Deadline = '{$deadline}' WHERE Current = true");
+		$result = mysqli_query($conn, "UPDATE Semester SET Deadline = '{$deadline}' WHERE Current = true");
 		
 		if ($result)
 		{
-			returnError(200,"Deadline set for {$deadline}");
+			echo "Deadline set for {$deadline}";
 		}
 		else
 		{
-			returnError(500, "Process Failed");
+			echo "Process Failed";
 		}
 
-		$stmt->close();
-		$conn->close();
 	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendJSON( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnError($code, $err )
-	{
-		$retValue = array(
-			"Error"=>array(
-				"code"=>$code,
-				"Message"=>$err));
-		
-		sendJSON(json_encode($retValue, JSON_FORCE_OBJECT));
-	}
+	header('Location: ../admin_homepage.php');
 	
 ?>
