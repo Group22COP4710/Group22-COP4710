677de63216ed4d0fcc56380203c01c5a8141d0e1
andregr13 <89662452+andregr13@users.noreply.github.com>
2021-12-06 20:20:56 -0500

    Removed JSON i/o and updated to Procedural Querying

--- api/changePassword.php
+++ api/changePassword.php
@@ -1,64 +1,44 @@
 <?php
 
-	$inData = getRequestInfo();
 
-	$userid = $inData["User_ID"];
-	$oldpass = $inData["oldPassword"];
-	$newpass = $inData["newPassword"];
-	$retype = $inData["retypePassword"];
+	$userid = $_POST["User_ID"];
+	$oldpass = $_POST["oldpass"];
+	$newpass = $_PASS["newpass"];
+	$retype = $_PASS["retypepass"];
 	
-	$conn = new mysqli("localhost", "user", "password", "final"); 	
+	$retVal;
+
+	$conn = mysqli_connect("localhost", "user", "password", "final"); 	
 	if( $conn->connect_error )
 	{
-		returnError(500, $conn->connect_error);
+		$retVal = $conn->connect_error;
 	}
 	else if ($newpass != $retype)
 	{
-		returnError(400, "Passwords don't match");
+		$retVal = "Passwords don't match";
 	}
 	else
 	{
-		$result = $conn->query("SELECT Password FROM Users WHERE User_ID = {$userid}");
+		$result = mysqli_query($conn, "SELECT Password FROM Users WHERE User_ID = {$userid}");
 		
-		if ($row = $result->fetch_assoc())
+		if ($row = mysqli_fetch_assoc($result))
 		{
 			if ($row["Password"] == $oldpass)
 			{
-				$result = $conn->query("UPDATE Users SET Password = '{$newpass}' WHERE User_ID = {$userid}");
-				returnError(200, "Password successfully changed");
+				$result = mysqli_query($conn, "UPDATE Users SET Password = '{$newpass}' WHERE User_ID = {$userid}");
+				$retVal =  "Password successfully changed";
 			}
 			else
 			{
-				returnError(400, "Old password incorrect");
+				$retVal =  "Old password incorrect";
 			}
 		}
 		else
 		{
-			returnError(500, "Error Occured");
+			$retVal =  "Error Occured";
 		}
 
 		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendJSON( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnError($code, $err )
-	{
-		$retValue = array(
-			"Error"=>array(
-				"code"=>$code,
-				"Message"=>$err));
-		
-		sendJSON(json_encode($retValue, JSON_FORCE_OBJECT));
 	}
 	
 ?>
